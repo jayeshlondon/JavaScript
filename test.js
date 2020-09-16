1112270922fdad3e50f7e7bf3f446ecc8a9c2871
Neha Patel <nehajp999@gmail.com>
2020-09-16 19:17:46 +0100

    new js file

---
+++ test.js
@@ -0,0 +1,15 @@
+var name = "Neha Jayesh Patel";
+console.log(name);
+10
+console.log(typeof(10));
+
+typeof(10.02)
+console.log(typeof(10.02));
+
+typeof(true)
+console.log(typeof(9));
+
+var a;
+a++;
+console.log(a);
+
